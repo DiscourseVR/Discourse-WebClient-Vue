cd494a5e50bcc9b9c07ac86b2d8c60587f9f272d
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-04-10 18:19:54 -0400

    sluig

---
+++ pages/videos/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-col>
+    <p>{{ filename }}</p>
+    <video
+      ref="videoPlayer"
+      controls
+    >
+      <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+    </video>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    filename: {
+      type: String,
+      default: 'File name'
+    }
+  },
+  head () {
+    return {
+      title: this.filename,
+      meta: [
+        { hid: 'description', name: 'description', content: 'The video' }
+      ]
+    }
+  }
+}
+</script>
