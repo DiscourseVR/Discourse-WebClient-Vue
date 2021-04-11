7fd99182062a3e70bac0092164faf7fcc17c9be9
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-04-10 21:04:36 -0400

    bubles

--- components/chatbubble.vue
+++ components/chatbubble.vue
@@ -1,8 +1,15 @@
 <template>
-  <div
-    class="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-3xl transition duration-500 ease-in-out hover:scale-110 transform"
-  >
-    It will be used for a full tutorial about building a chat app with vue, tailwind and firebase.
+  <div>
+    <div
+      class="bg-gray-300 w-72 mx-4 my-2 p-2 rounded-3xl transition duration-500 ease-in-out hover:scale-110 transform"
+    >
+      {{ message }}
+    </div>
+    <div
+      class="font-thin float-right"
+    >
+      {{ timestamp }}
+    </div>
   </div>
 </template>
 
